4e244d26f11bcb188998f49c6988113af362ca23
ReeyaDono <metrytmc@gmail.com>
2021-10-26 09:56:09 +0700

    [tracker] Implement basic charts with chart.js

--- tracker/static/script.js
+++ tracker/static/script.js
@@ -8,6 +8,29 @@ $(document).ready(function(){
         $('#jumlah_meninggal').text(data.meninggal);
     });
 
+    $.getJSON("http://apicovid19indonesia-v2.vercel.app/api/indonesia/harian", 
+    function(data){
+        const dataset = [];
+        const dateset = [];
+        for (let i = data.length-7; i < data.length; i++){
+            dataset.push(data[i].positif_kumulatif);
+            dateset.push(data[i].tanggal);
+        }
+
+        const indo_chart = document.getElementById('indo_chart').getContext('2d');
+
+        const new_chart = new Chart(indo_chart, {
+            type: 'line',
+            data: {
+                labels: dateset,
+                datasets: [{
+                    label: 'positif',
+                    data: dataset
+                }]
+            }
+        })
+    });
+
     $("#button_prov").click(function(){
         const inp = $("#input_prov").val();
         $.getJSON("https://apicovid19indonesia-v2.vercel.app/api/indonesia/provinsi", 
