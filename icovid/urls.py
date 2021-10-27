dcf96267201b70c76fa1a00ab87f06103086d38f
Najwa Kariza Anjelia <najwa.kariza12@gmail.com>
2021-10-27 16:23:35 +0700

    add userprofile

--- icovid/urls.py
+++ icovid/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import index
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('profileapp/',include('profileapp.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
