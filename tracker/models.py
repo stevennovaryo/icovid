0f45580127edf77dda2eae67872de923bea5b592
ReeyaDono <metrytmc@gmail.com>
2021-10-28 14:50:04 +0700

    [tracker] Alter chart filter model

--- tracker/models.py
+++ tracker/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class TrackerFilter(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     chart_type = models.IntegerField(default=1)
     number_of_data = models.IntegerField(default=7)
