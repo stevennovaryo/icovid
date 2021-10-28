ceb0fbfdb1f1a8bd33ab5ec179522a8cd6c1566a
ReeyaDono <metrytmc@gmail.com>
2021-10-28 15:50:32 +0700

    [tracker] Add basic form for char filter

--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,9 @@
 from django.http import response
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from .forms import FilterForm
 
 # Create your views here.
 def index(request):
-    response = {"number_of_data" : 7, "chart_type" : 2}
+    response = {"number_of_data" : 7, "chart_type" : 2, "form" : FilterForm()}
     return render(request, "tracker_index.html", response)
