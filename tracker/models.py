37d0db0eca47259c09c177ea33b9cc5156a79048
ReeyaDono <metrytmc@gmail.com>
2021-10-28 13:35:34 +0700

    [tracker] Create model for user filter

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TrackerFilter(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chart_type = models.IntegerField(default=1)
+    number_of_data = models.IntegerField(default=7)
