8818ef14050c3dfe56a06c00c479e2ed2556b6b2
Izzan Nufail <inarvin33@gmail.com>
2021-11-03 23:39:26 +0700

    Buat Ciella

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import index, read, postArticle
+from .views import index, read, postArticle, load_more
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('add/', postArticle)
+    path('', index, name = 'index'),
+    path('add/', postArticle, name = 'postArticle'),
+    path('<str:id>/', read, name = 'read'),
+    path('load-more',load_more,name='load-more'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/views.py
+++ news/views.py
@@ -1,24 +1,31 @@
+import collections
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
 from news.forms import articleForm
 from news.models import Article
+from django.core import serializers
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
+
     articleList = list(Article.objects.all())
     firstArticle = articleList[len(articleList) - 1]
-    remainingArticles = articleList[:len(articleList) - 1]
-    remainingArticles = remainingArticles[::-1]
+    nextThreeArticles = articleList[len(articleList) - 4:len(articleList) - 1]
+    nextThreeArticles = nextThreeArticles[::-1]
 
-    response = {
+    context = {
         'firstArticle' : firstArticle,
-        'remainingArticles' : remainingArticles
+        'nextThreeArticles' : nextThreeArticles,
     }
-    return render(request, 'news-home.html', response)
+    return render(request, 'news-home.html', context)
 
-def read(request):
-    return render(request, 'news-read.html')
+def read(request, id):
+    article = Article.objects.get(title=id)
+    context = {
+        'article' : article
+    }
+    return render(request, 'news-read.html', context)
 
 @login_required(login_url='/admin/login/')
 def postArticle(request):
@@ -32,3 +39,21 @@ def postArticle(request):
         form = articleForm()
     context['form'] = form
     return render(request, "article-post.html", context)
+
+def load_more(request):
+
+    articleList = list(Article.objects.all())
+    articleQuer = Article.objects.all()
+    articleQuer = articleQuer[::-1]
+    articleQuer = articleQuer[1::]
+
+    offset=int(request.POST['offset'])
+    limit=3
+    posts=articleQuer[offset:limit+offset]
+    totalData=Article.objects.count()
+    data={}
+    posts_json=serializers.serialize('json', posts)
+    return JsonResponse(data={
+        'posts':posts_json,
+        'totalResult':totalData
+    })
