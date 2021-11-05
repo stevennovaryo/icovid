4c91fc3ed706f3ffc743fafc457b44da6017948b
Graciella Regina <74176164+ciellaindria28@users.noreply.github.com>
2021-11-05 18:33:24 +0700

    Fix Home (#19)
    * [ultilities] Implemented ultilities app
    
    * create home module
    
    * Created News Branch
    
    * change in urls.py and home2.html
    
    * [ultilities] fix navbar
    
    * [utilities] fix navbar,log,announcement
    
    * commit
    
    * Buat Ciella
    
    * updates on views and reqs
    
    * [utilities] fix navbar guest and ajax
    
    * [utilities] add icovid log sample
    
    * update on views
    
    * [utilities] fix navbar
    
    * test
    
    * [utilities] fix navbar and announcement
    
    * Fixed bug in views
    
    * [utilities] Imitate branch master
    
    * commit
    
    * delete previous navbar
    
    * updatesss
    
    * home and news
    
    * delete some pictures
    
    * remove images
    
    * Final Changes to news
    
    * fix some issues
    
    * remove some ununsed images
    
    Co-authored-by: Hanif Syuaib <hanifsyuaib@gmail.com>
    Co-authored-by: Izzan Nufail <inarvin33@gmail.com>
    Co-authored-by: hanifsyuaib <89407284+hanifsyuaib@users.noreply.github.com>
    Co-authored-by: izzannuf <izzan.nufail@ui.ac.id>

--- home/views.py
+++ home/views.py
@@ -7,19 +7,14 @@ from django.contrib.auth.models import User
 
 
 def index(request):
-    
+  
     feedbacks = Feedback.objects.all().order_by("-created_at")[:4]
     feedbacksAll = Feedback.objects.filter(user_id = request.user.id)
     articleList = Article.objects.all()[::-1]
-    # articleList = articleList[:3]
-    firstArticle = articleList[0]
-    secondArticle = articleList[1]
-    thirdArticle = articleList[2]
-    firstURL = firstArticle.title.replace(" ","%20") 
-    secondURL = secondArticle.title.replace(" ","%20") 
-    thirdURL = thirdArticle.title.replace(" ","%20") 
-    context ={'feedbacks':feedbacks,'feedbacksAll':feedbacksAll,'firstArticle':firstArticle,'secondArticle':secondArticle,'thirdArticle':thirdArticle,'firstURL':firstURL,'secondURL':secondURL,
-    'thirdURL':thirdURL}
+    context ={} 
+    articleList = articleList[:3]
+    context ={'feedbacks':feedbacks,'feedbacksAll':feedbacksAll,'articleList':articleList}
+
     # create object of form
     form = FeedbackForm(request.POST or None, request.FILES or None)
       
--- icovid/urls.py
+++ icovid/urls.py
@@ -16,21 +16,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .views import index
-import tracker.urls as tracker
 from django.conf.urls.static import static
 from django.conf import settings
 from .views import index
+import news.urls as news
+import tracker.urls as tracker
+from django.conf import settings
 import forum.urls as forum
 import utilities.urls as utilities
 
 
 urlpatterns = [
-    path('', index),
-    path('forum/', include(forum)),
     path('admin/', admin.site.urls),
-    path('tracker/', include(tracker)),
+    # path('news/', include(news)),
+    path('auth/', include(('authentication.urls', 'authentication'), namespace='authentication')),
     path('administrator/', include('utilities.urls')),
     path('profileapp/',include('profileapp.urls')),
+    path('', index),
+    path('forum/', include(forum)),
+    path('tracker/', include(tracker)),
     path('auth/', include(('authentication.urls', 'authentication'), namespace='authentication')),
     path('news/', include(('news.urls', 'news'), namespace='news')),
     path('home/', include(('home.urls', 'home'), namespace='home/')),
