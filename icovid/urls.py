97a465f814d8664f828dcedf481d1d371005918f
Muhammad Kenshin Himura Mahmuddin <70798188+ReeyaDono@users.noreply.github.com>
2021-11-05 14:33:43 +0700

    Covid tracker (#11)
    * [tracker] Start tracker app
    
    * [tracker] Implement template and basic tracker
    
    * [tracker] Implement data checker for province
    
    * [tracker] Implement login only for province data
    
    * [tracker] Add toLowerCase for province name check
    
    * [tracker] Implement basic charts with chart.js
    
    * [tracker] Add commas to number display
    
    * [tracker] Add more dataset to chart
    
    * [tracker] Add minor adjustment for province data checker
    
    * [tracker] Add dataset chooser for chart
    
    * [tracker] Add new chart
    
    * [tracker] Create model for user filter
    
    * [tracker] Add response in views to control chart display
    
    * [tracker] Alter chart filter model
    
    * [tracker] Add basic form for char filter
    
    * [tracker] Implement backend for tracker filter
    
    * [tracker] Add minor adjustment for POST request handling
    
    * [tracker] fix a bit more conflict
    
    * [tracker] Change layout
    
    * [tracker] Debug mode on
    
    * [tracker] Add navbar
    
    * [tracker] change title

--- icovid/urls.py
+++ icovid/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
+from .views import index
+import tracker.urls as tracker
 from django.conf.urls.static import static
 from django.conf import settings
 from .views import index
@@ -24,12 +26,12 @@ import utilities.urls as utilities
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('tracker/', include(tracker)),
+    path('administrator/', include('utilities.urls')),
     path('profileapp/',include('profileapp.urls')),
     path('auth/', include(('authentication.urls', 'authentication'), namespace='authentication')),
-    path('administrator/', include('utilities.urls')),
     path('news/', include(('news.urls', 'news'), namespace='news')),
     path('home/', include(('home.urls', 'home'), namespace='home/')),
-    path('', index)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ tracker/static/script.js
@@ -0,0 +1,193 @@
+$(document).ready(function(){
+    $("#prov_container").hide();
+    let new_chart = null;
+    let new_chart2 = null;
+    let active_chart = 'positif';
+
+    $.getJSON("https://apicovid19indonesia-v2.vercel.app/api/indonesia", 
+    function(data){
+        $('#jumlah_positif').text(data.positif.toLocaleString("en-US"));
+        $('#jumlah_sembuh').text(data.sembuh.toLocaleString("en-US"));
+        $('#jumlah_meninggal').text(data.meninggal.toLocaleString("en-US"));
+    });
+
+    $.getJSON("http://apicovid19indonesia-v2.vercel.app/api/indonesia/harian", 
+    function(data){
+        const dataset_positif = [];
+        const dataset_sembuh = [];
+        const dataset_meninggal = [];
+        const dataset_positif2 = [];
+        const dataset_sembuh2 = [];
+        const dataset_meninggal2 = [];
+        const dateset = [];
+        for (let i = data.length-dataCount; i < data.length; i++){
+            dataset_positif.push(data[i].positif_kumulatif);
+            dataset_sembuh.push(data[i].sembuh_kumulatif);
+            dataset_meninggal.push(data[i].meninggal_kumulatif);
+            dataset_positif2.push(data[i].positif);
+            dataset_sembuh2.push(data[i].sembuh);
+            dataset_meninggal2.push(data[i].meninggal);
+            dateset.push(data[i].tanggal.split('T')[0]);
+        }
+
+        let indo_chart = document.getElementById('indo_chart');
+        let indo_chart2 = document.getElementById('indo_chart2');
+        if (indo_chart != null){
+            indo_chart = indo_chart.getContext('2d');
+        }
+        if (indo_chart2 != null){
+            indo_chart2 = indo_chart2.getContext('2d');
+        }
+
+        if (indo_chart != null){
+            new_chart = new Chart(indo_chart, {
+                type: 'line',
+                data: {
+                    labels: dateset,
+                    datasets: [{
+                        label: 'Positif',
+                        data: dataset_positif,
+                        backgroundColor: 'rgb(255, 188, 188)',
+                        borderColor: 'rgb(255, 188, 188)'
+                    },{
+                        label: 'Sembuh',
+                        data: dataset_sembuh,
+                        backgroundColor: 'rgb(40, 255, 191)',
+                        borderColor: 'rgb(40, 255, 191)',
+                        hidden: true
+                    },{
+                        label: 'Meninggal',
+                        data: dataset_meninggal,
+                        backgroundColor: 'rgb(243, 139, 160)',
+                        borderColor: 'rgb(243, 139, 160)',
+                        hidden: true
+                    }]
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        title: {
+                            display: true,
+                            text: "Grafik Kumulatif Harian"
+                        }
+                    }
+                }
+            })
+        }
+        if (indo_chart2 != null){
+            new_chart2 = new Chart(indo_chart2, {
+                type: 'line',
+                data: {
+                    labels: dateset,
+                    datasets: [{
+                        label: 'Positif',
+                        data: dataset_positif2,
+                        backgroundColor: 'rgb(255, 188, 188)',
+                        borderColor: 'rgb(255, 188, 188)'
+                    },{
+                        label: 'Sembuh',
+                        data: dataset_sembuh2,
+                        backgroundColor: 'rgb(40, 255, 191)',
+                        borderColor: 'rgb(40, 255, 191)',
+                        hidden: true
+                    },{
+                        label: 'Meninggal',
+                        data: dataset_meninggal2,
+                        backgroundColor: 'rgb(243, 139, 160)',
+                        borderColor: 'rgb(243, 139, 160)',
+                        hidden: true
+                    }]
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        title: {
+                            display: true,
+                            text: "Grafik Harian"
+                        }
+                    }
+                }
+            })
+        }
+        
+    });
+
+    $("#btnradio1").click(function(){
+        if (active_chart != 'positif'){
+            active_chart = 'positif';
+            if (new_chart != null){
+                new_chart.data.datasets[0].hidden = false;
+                new_chart.data.datasets[1].hidden = true;
+                new_chart.data.datasets[2].hidden = true;
+                new_chart.update();
+            }
+            if (new_chart2 != null){
+                new_chart2.data.datasets[0].hidden = false;
+                new_chart2.data.datasets[1].hidden = true;
+                new_chart2.data.datasets[2].hidden = true;
+                new_chart2.update();
+            }  
+        }
+    })
+
+    $("#btnradio2").click(function(){
+        if (active_chart != 'sembuh'){
+            active_chart = 'sembuh';
+            if (new_chart != null){
+                new_chart.data.datasets[0].hidden = true;
+                new_chart.data.datasets[1].hidden = false;
+                new_chart.data.datasets[2].hidden = true;
+                new_chart.update();
+            }
+            if (new_chart2 != null){
+                new_chart2.data.datasets[0].hidden = true;
+                new_chart2.data.datasets[1].hidden = false;
+                new_chart2.data.datasets[2].hidden = true;
+                new_chart2.update();
+            } 
+        }
+    })
+
+    $("#btnradio3").click(function(){
+        if (active_chart != 'meninggal'){
+            active_chart = 'meninggal';
+            if (new_chart != null){
+                new_chart.data.datasets[0].hidden = true;
+                new_chart.data.datasets[1].hidden = true;
+                new_chart.data.datasets[2].hidden = false;
+                new_chart.update();
+            }
+            if (new_chart2 != null){
+                new_chart2.data.datasets[0].hidden = true;
+                new_chart2.data.datasets[1].hidden = true;
+                new_chart2.data.datasets[2].hidden = false;
+                new_chart2.update();
+            } 
+        }
+    })
+
+    $("#button_prov").click(function(){
+        const inp = $("#input_prov").val();
+        $.getJSON("https://apicovid19indonesia-v2.vercel.app/api/indonesia/provinsi", 
+        function(data){
+            let found = false;
+            for (let i in data){
+                if (data[i].provinsi.toLowerCase() === inp.toLowerCase()){
+                    $('#prov-name').text(data[i].provinsi);
+                    $('#jumlah_positif_prov').text(data[i].kasus.toLocaleString("en-US"));
+                    $('#jumlah_sembuh_prov').text(data[i].sembuh.toLocaleString("en-US"));
+                    $('#jumlah_meninggal_prov').text(data[i].meninggal.toLocaleString("en-US"));
+                    found = true;
+                    break;
+                }
+            }
+            if (found){
+                $("#prov_container").show();
+            }
+        });
+    });
+});
