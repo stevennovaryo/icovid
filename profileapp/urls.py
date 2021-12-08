d694aa4b8bed15c45bc09d4631c3c2ee1153438f
Najwa Kariza Anjelia <najwa.kariza12@gmail.com>
2021-12-08 16:02:38 +0700

    update json url

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('profile-page/', views.profile, name='user-profile'),
     path('edit-profile/', views.profileupdate, name='update-profile'), 
-    path('profile-datas/', views.profiledatas, name='profile-datas'),
+    path('profile-page/profile-datas/', views.profiledatas, name='profile-datas'),
     # path('upload/', views.uploadavatar, name='upload-avatar'),
     path('profile/<int:id>', views.otheruserprofile, name='otherprofile'),
 ]
