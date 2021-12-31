4ff7e9780c191986c96ab9476e9320303774df24
gbhisma <bhismagregorius@gmail.com>
2021-12-31 10:46:31 +0700

    changed api urls

--- forum/urls.py
+++ forum/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', index, name='Forum'),
     path('postToForum/',post_to_forum,name='postToForum'),
     path('posts/<str:slug>/', forum_post_detail, name="detail"),
-    path('getForumList/', get_forum_list, name="getForumList"),
+    path('api/ForumHome/', get_forum_list, name="getForumList"),
 ]
